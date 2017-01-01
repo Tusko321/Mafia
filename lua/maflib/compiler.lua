54624f5b33e2e17cb8bcc245be224a78f4f5005c
swog <sanicsupersonic@gmail.com>
2016-12-31 20:36:24 -0600

    Update compiler.lua

--- lua/maflib/compiler.lua
+++ lua/maflib/compiler.lua
@@ -1,3 +1,13 @@
+maflib.global = {};
+
+function maflib:AddGlobal(type, name, value)
+	maflib.global["name"] = {type = type, value = value};
+end
+
+function maflib:RemoveGlobal(name)
+	
+end
+
 function maflib:CompileLanguage(name, text)
 	local comments = maflib.languages[name].comments;
 	local advcomments = maflib.languages[name].advcomments;
