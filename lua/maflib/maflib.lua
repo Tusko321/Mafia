0bb8984b14439264333c1ca669052c14765b449a
swog <sanicsupersonic@gmail.com>
2016-12-31 19:48:21 -0600

    Update maflib.lua

--- lua/maflib/maflib.lua
+++ lua/maflib/maflib.lua
@@ -1,6 +1,15 @@
 local maflib = {};
 maflib.languages = {};
+maflib.languagebase = {
+	function SetComments(...)
+		maflib.languages[self.name]["comments"]	= {...};
+	end
+};
 
 function maflib:AddLanguage(name)
-	maflib.languages[name] = {};
+	self.languages[name] = {};
+	self.languages[name].base = maflib.languagebase;
+	self.languages[name].base["name"] = name;
+	
+	return (self.languagebase);
 end
