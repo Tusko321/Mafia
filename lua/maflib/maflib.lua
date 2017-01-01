890157a1dc9a8c9c40be75d28e80b8d636259c1c
swog <sanicsupersonic@gmail.com>
2016-12-31 20:05:05 -0600

    Update maflib.lua

--- lua/maflib/maflib.lua
+++ lua/maflib/maflib.lua
@@ -6,6 +6,10 @@ function maflib.languagebase:SetComments(...)
 	maflib.languages[self.name].comments = {...};
 end
 
+function maflib.languagebase:SetTypes(...)
+	maflib.languages[self.name].types = {...};
+end
+
 function maflib.languagebase:SetAdvancedComments(...)
 	for k,v in next, {...} do
 		maflib.languages[self.language].advcomments[maflib.languages[self.language].advcomments + 1] = {v[1], v[2]};
