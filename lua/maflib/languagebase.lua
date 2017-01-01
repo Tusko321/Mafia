86bd27d43cf865973ca1cb35ff7a31d48e7f2b33
swog <sanicsupersonic@gmail.com>
2016-12-31 20:10:31 -0600

    Update languagebase.lua

--- lua/maflib/languagebase.lua
+++ lua/maflib/languagebase.lua
@@ -5,15 +5,19 @@ function maflib.languagebase:SetComments(...)
 end
 
 function maflib.languageBase:AddComment(comment)
-	maflib.languages[self.name].comments[maflib.languages[self.name].comments + 1] = comment;
+	maflib.languages[self.name].comments[#maflib.languages[self.name].comments + 1] = comment;
+end
+
+function maflib.languagebase:SetAdvancedComments(...)
+	for k,v in next, {...} do
+		maflib.languages[self.language].advcomments[#maflib.languages[self.language].advcomments + 1] = {v[1], v[2]};
+	end
 end
 
 function maflib.languagebase:SetTypes(...)
 	maflib.languages[self.name].types = {...};
 end
 
-function maflib.languagebase:SetAdvancedComments(...)
-	for k,v in next, {...} do
-		maflib.languages[self.language].advcomments[maflib.languages[self.language].advcomments + 1] = {v[1], v[2]};
-	end
+function maflib.languageBase:AddType(typename)
+	maflib.languages[self.name].types[#maflib.languages[self.name].types + 1] = typename;	
 end
