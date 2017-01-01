b3327717fd3ec96e298b61d38cb3f389a3db6243
swog <sanicsupersonic@gmail.com>
2016-12-31 20:19:39 -0600

    Update compiler.lua

--- lua/maflib/compiler.lua
+++ lua/maflib/compiler.lua
@@ -1,4 +1,5 @@
 function maflib:CompileLanguage(name, text)
 	local comments = maflib.languages[name].comments;
 	local advcomments = maflib.languages[name].advcomments;
+	local types = maflib.languages[name].types;
 end
