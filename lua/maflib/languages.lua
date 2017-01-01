9daa6a953da718a7da039e0ce8d7b945bb5ca238
swog <sanicsupersonic@gmail.com>
2016-12-31 20:18:18 -0600

    Update languages.lua

--- lua/maflib/languages.lua
+++ lua/maflib/languages.lua
@@ -1,4 +1,10 @@
+--[[
+Add your languages here
+C++ is already created for you
+]]--
+
 local cpp = maflib:AddLanguage("C++");
 cpp:AddComment("//");
 cpp:AddAdvancedComment("/*", "*/");
 cpp:SetTypes("int", "float", "bool");
+cpp:AddExtension("cpp");
